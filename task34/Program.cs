4f5d748adcc2ef1b8e4fba4dd001ce74ccd1cbec
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-12-10 07:33:16 +0700

    / add  commit task34

---
+++ task34/Program.cs
@@ -0,0 +1,30 @@
+﻿// task33 задайте массив заполненный случайными положительными трехзначными
+// числами. Наапишите программу, которая покажет количество четных чисел в 
+// массиве. [345, 897, 568, 234] ->2
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+int ReleaseArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine()); 
+int[] array = new int[n]; 
+InputArray(array);
+Console.WriteLine($"Исходный массив: [{string.Join(", ", array)}]"); 
+Console.WriteLine($"Результат: {ReleaseArray(array)}");
+
+
